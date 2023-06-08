92378af9aebb1ec71e5437e756a0d0793c02c909
achyuthask <96536973+achyuthask@users.noreply.github.com>
2023-06-08 12:53:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './navbar/navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     < img src='https://store-images.s-microsoft.com/image/apps.33776.13570837168441901.d8820ad6-c4ef-45a9-accb-c6dd763aee48.560134ce-5fa0-4486-95cd-b0ba8d4921ff' alt=" " width="100%"  height="100%" />
+     <div className='centered' > <h1>Home</h1></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/navbar/navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div>
+    <div>
+        
+    </div>
+    <ul>
+    <h3 style={{float:"left",color:'white'}}>LOGO</h3>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+      
+    </div>
+  )
+}
+
+export default Navbar;
